816554c673ccd7b3baf1bfcf9927bb0f73729536
CuervoCEO <jcuervo037@soyudemedellin.edu.co>
2024-10-17 13:29:51 -0500

    Progreso hasta ahora :)

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -1,1 +1,20 @@
-# TODO: Implementa el código del ejercicio aquí
+from abc import ABC, abstractmethod 
+#Librerias necesarias para crear clases y  métodos abstractos
+
+class ReglaValidacion(ABC): #hereda de ABC para que sea una clase abstracta
+    
+    
+    #Según el ejercicio:  el atributo _longitud_esperada, el cual es inicializado en el constructor de la clase e indica la longitud que la clave debe superar.
+    def __init__(self, longitud_esperada: int):
+        self.longitud_esperada = longitud_esperada
+
+    @abstractmethod #El decorador que indica los métodos abstractos
+    def es_valida(self, clave: str) -> bool:
+        pass 
+
+    def _validar_longitud(self, clave: str,) -> bool:
+        return len(clave) >= self.longitud_esperada
+
+
+
+
